bf67f4d5815052430353e7e918b6ea15ce479cda
jamesarrojo <jamesarrojo.01@gmail.com>
2022-08-06 00:02:19 +0800

    Add solution to problems 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    let repeated = str
+    
+    for (let i = 0; i < n - 1; i++){
+        repeated += str
+    }
 
+    return repeated
 };
 
 // Do not edit below this line
