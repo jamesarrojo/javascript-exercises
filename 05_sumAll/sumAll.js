d6d0f58effa85e5127a3b754fd4b129c3aed41dc
jamesarrojo <jamesarrojo.01@gmail.com>
2022-08-06 23:40:09 +0800

    Add solution for challenge 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    
+    let biggerNum = y
+    let smallerNum = x
 
+    if (x > y) {
+        biggerNum = x
+        smallerNum = y
+    }
+    
+    let sum = smallerNum
+    for (let i = 2; i <= biggerNum; i++) {
+        sum += i
+    }
+
+    return sum
 };
 
 // Do not edit below this line
