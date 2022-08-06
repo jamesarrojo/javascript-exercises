04e6825ba824cdab589b24b7e72ea06362998ee7
jamesarrojo <jamesarrojo.01@gmail.com>
2022-08-06 23:59:12 +0800

    Add solution to challenge 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+  let celcius = (temp - 32) * 5 / 9
 
+  return Number.parseFloat(celcius.toFixed(1))
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  let fahrenheit = (temp * 9 / 5) + 32
 
+  return Number.parseFloat(fahrenheit.toFixed(1))
 };
 
 // Do not edit below this line
