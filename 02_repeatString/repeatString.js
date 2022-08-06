05168b07322991b14bd6ab94b0f444c59e1a4cc7
jamesarrojo <jamesarrojo.01@gmail.com>
2022-08-06 14:48:24 +0800

    Fix challenge 2 code and add solution to challenge 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
 const repeatString = function(str, n) {
     let repeated = str
     
+    if (n === 0) return ""
+
+    if (n < 0) return "ERROR"
+
     for (let i = 0; i < n - 1; i++){
         repeated += str
     }
